c19f5e23a5a268cb77eb9431103a5606ddd5ff69
Harsh <83541303+harshdhankhar10@users.noreply.github.com>
2024-11-18 00:21:12 +0530

    visitor tracking system implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import Routes from './pages/Routes'
 import "./App.css"
+import axios from 'axios'
 
 const App = () => {
+  useEffect(()=>{
+    const fetchVisitor = async () => {
+      try {
+        const userAgent = navigator.userAgent;
+        const ipAddress = await axios.get("https://api.ipify.org?format=json");
+
+        await axios.post(`${import.meta.env.VITE_REACT_APP_API}/api/v1/visitor/log-visitor`, {
+          ipAddress: ipAddress.data.ip,
+          userAgent: userAgent
+        })
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    fetchVisitor()
+
+  },[])
   return (
     <>
      <div >
