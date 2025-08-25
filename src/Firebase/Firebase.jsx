ee5a8b05439b3fb12ebffcf1c62af4af963d15de
Harsh <83541303+harshdhankhar10@users.noreply.github.com>
2025-08-25 12:10:28 +0530

    Update Firebase config to use environment variables

--- src/Firebase/Firebase.jsx
+++ src/Firebase/Firebase.jsx
@@ -8,14 +8,16 @@ import { getStorage } from "firebase/storage";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCJzrd4RcbAM9mNMtqyuDoaMVwKK_ZA5PY",
-  authDomain: "buckettalk-ff0b3.firebaseapp.com",
-  projectId: "buckettalk-ff0b3",
-  storageBucket: "buckettalk-ff0b3.appspot.com",
-  messagingSenderId: "811070514736",
-  appId: "1:811070514736:web:dd95897d6990af19efeff5",
-  measurementId: "G-QEP0T4CFEF"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
+
+export default firebaseConfig;
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
